7467c3e17a7105927b6ba430ca172109528e08d6
ich777 <ich777@noreply.example.org>
2020-08-17 18:03:03 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 INSPIRCD_ROOT="${DATA_DIR}"
-sleep infinity
 
-## TODO
-cd ${DATA_DIR}
-wget -q -nc --show-progress --progress=bar:force:noscroll -O OUTPUT "URL"
-tar -C ${DATA_DIR} --strip-components=1 -xf ${DATA_DIR}/InspIRCd-v*.tar.gz
-rm ${DATA_DIR}/InspIRCd-v*.tar.gz
+LAT_V="$(curl -s https://api.github.com/repos/ich777/docker-inspircd/releases/latest | grep tag_name | cut -d '"' -f4)"
+CUR_V="$(${DATA_DIR}/bin/inspircd --version 2>/dev/null | grep "InspIRCd-" | cut -d '-' -f2)"
+
+echo "---Version Check---"
+if [ -z "$CUR_V" ]; then
+	echo "---InspIRCd not found, downloading and installing v$LAT_V...---"
+	cd ${DATA_DIR}
+	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/InspIRCd-v$LAT_V.tar.gz "https://github.com/ich777/docker-inspircd/releases/download/$LAT_V/InspIRCd-v$LAT_V.tar.gz" ; then
+		echo "---Successfully downloaded InspIRCd v$LAT_V---"
+	else
+		echo "---Something went wrong, can't download InspIRCd v$LAT_V, putting container into sleep mode!---"
+		sleep infinity
+	fi
+	tar -C ${DATA_DIR} --strip-components=1 -xf ${DATA_DIR}/InspIRCd-v$LAT_V.tar.gz
+	rm ${DATA_DIR}/InspIRCd-v$LAT_V.tar.gz
+elif [ "$CUR_V" != "$LAT_V" ]; then
+	echo "---Version missmatch, installed v$CUR_V, downloading and installing latest v$LAT_V...---"
+	cd ${DATA_DIR}
+	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/InspIRCd-v$LAT_V.tar.gz "https://github.com/ich777/docker-inspircd/releases/download/$LAT_V/InspIRCd-v$LAT_V.tar.gz" ; then
+		echo "---Successfully downloaded InspIRCd v$LAT_V---"
+	else
+		echo "---Something went wrong, can't download InspIRCd v$LAT_V, putting container into sleep mode!---"
+		sleep infinity
+	fi
+	echo "---Moving configruation---"
+	mv ${DATA_DIR}/conf/ /tmp/
+	tar -C ${DATA_DIR} --strip-components=1 --overwrite -xf ${DATA_DIR}/InspIRCd-v$LAT_V.tar.gz
+	rm ${DATA_DIR}/InspIRCd-v$LAT_V.tar.gz
+	echo "---Restoring configuration---"
+	rm -R ${DATA_DIR}/conf
+	mv /tmp/conf/ ${DATA_DIR}/
+elif [ "$CUR_V" == "$LAT_V" ]; then
+	echo "---InspIRCd v$CUR_V up-to-date---"
+fi
 
 echo "---Prepare Server---"
 echo "---Checking if TLS Certificate is in place...---"
@@ -45,6 +73,9 @@ if [ ! -e $INSPIRCD_ROOT/conf/dhparams.pem ]; then
 else
     echo "---Diffie–Hellman parameters file found, continuing!---"
 fi
+
+chmod -R ${DATA_PERM} ${DATA_DIR}
+echo "---Server ready---"
 
 echo "---Starting InspIRCd---"
 cd $INSPIRCD_ROOT
